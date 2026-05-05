a6cf78c917b378a56cb20a646846055ddfc1289d
DragonIsGone <rtclemow@icloud.com>
2026-05-05 10:46:53 +1000

    Feat: Output detection system, a bit of STT system

--- src/stt.rs
+++ src/stt.rs
@@ -1,3 +1,11 @@
-pub fn rec_input() -> String {
-    
+use vosk::{Model, Recognizer};
+
+pub fn bit_test() {
+    let model = Model::new("model").expect("Model Not foud in ~/models");
+
+    let recognizer = Recognizer::new(&model, 16000.0).expect("Failed to create recogniser");
+
+    println!("STT: Model and Recogniser");
 }
+
+pub fn rec_input() -> String {}
